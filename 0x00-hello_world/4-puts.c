ccefac4ed032b178af50db19eeab80d005a2c745
Ibrahim Ridwanullah <ridwanullahibrahim112@gmail.com>
2022-09-09 01:44:15 -0700

    C program that print some phrase

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *print the phrase "Programming is like
+ *building a multilingual puzzle
+ *return 0 is successful and non-zero otherwise
+ */
+
+int main (void)
+{
+  putc("\"Programming is like building a multilingual puzzle\n");
+    return(0);
+}    
