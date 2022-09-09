c9912f754da7f5396c3137f69fa025cdf85fb053
Ibrahim Ridwanullah <ridwanullahibrahim112@gmail.com>
2022-09-09 02:18:56 -0700

    print a phrase using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *main: print the phrase with proper grammar, but the outcome is a piece of art,
- *return: 0 is successful or non-zero otherwise
- */
-
-int main()
+*main: print the phrase with proper grammar, but the outcome is a piece of art,
+*return: 0 is successful or non-zero otherwise
+*/
+int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");
-  return(0);
+  return (0);
 }
