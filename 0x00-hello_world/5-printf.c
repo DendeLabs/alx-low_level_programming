dec6791117639ab7f52d941e7116f147dae6a264
Ibrahim Ridwanullah <ridwanullahibrahim112@gmail.com>
2022-09-09 22:59:22 -0700

    print the size of the various data type

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
-*main: print the phrase with proper grammar, but the outcome is a piece of art,
+*main: print the string in the printf function
+*
+*Desription: print the phrase with proper grammar, but the outcome is a piece of art,
 *return 0 if successful or non-zero otherwise
 */
 int main(void)
